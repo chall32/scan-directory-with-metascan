51b30734b11427eccea501c3b1af61f131032249
Chris Hall <chall32@gmail.com>
2015-07-26 11:51:23 +0100

    v1.0 Initial Release

---
+++ README.md
@@ -0,0 +1,68 @@
+scan-directory-with-metascan
+============================
+Chris Hall - [chall32.blogspot.com] 
+Metascan - [www.metascan-online.com]
+
+### What does this script do?
+Scan a folder of files using Metascan's [Public API].
+The script is can also be used as [SABnzbd postprocessing script].
+
+**The script performs the following:**
+
+For each file in the folder to be scanned,
+
+1.  Create a MD5 hash of the file
+2.  Post the MD5 hash value to metascan via the Metascan public API
+3.  Categorise the API return value (categories below)
+4.  Report results
+5.  If file extension is on the risky file list, upload file to Metascan via public API for analysis
+6.  Wait for file analysis to complete
+7.  Re-post the MD5 hash value to Metascan via the public API
+8.  Categorise the API return value (categories below)
+9.  Rename file if file is found infected or suspicious
+10.  Report results
+
+**Script Return Categories:**
+
++ VIRUS FOUND!! - Fairly obvious. Metascan knows that this file is infected with a virus. Infected file's filename is appended with "__INFECTED"
++ SUSPICIOUS FILE!! - Metascan knows that this file is suspicious. Suspicious file's filename is appended with "__SUSPICIOUS"
++ All OK - no virus found - Metascan knows that this file is OK. All good
++ File(s) not known to Metascan - Metascan has not seen this file before
++ Problem Scanning!! - Something went wrong
+
+See Metascan's [API definitions] for furthyer information.
+
+### How to Use the Script
+1.  Each API call requires a Metascan Online API key. To obtain your free Metascan Online API key, follow the instructions at Metascan's [Public API] page
+2.  Enter your API key into your downloaded copy of the script betweeen the quotes into the variable ```myapikey```
+3.  Review the ```extlist``` variable.  Add or remove any file extensions to be scanned as required.  Files with extensions matching those in this list will be uploaded to Metascan for analysis
+4.  Save and close the script
+
+### How to Use the Script
+Simply:
+```python scan-directory-with-metascan.py <path to directory to be scanned>```
+
+For example:
+```python scan-directory-with-metascan.py /home/chris/files```
+
+For those running under Linux, dont forget to ```chmod +x scan-directory-with-metascan.py``` to mark the file as an executable first.
+### More about Metascan
+[![Metascan Video](http://img.youtube.com/vi/rNqwlpuraaI/0.jpg)](http://www.youtube.com/watch?v=rNqwlpuraaI)
+(Click to watch video on YouTube)
+
+### What's New?
+***See the [changelog] for what's new in the most recent release.***
+
+### [Click here to download latest version](https://github.com/chall32/scan-directory-with-metascan/blob/master/scan-directory-with-metascan.py?raw=true)
+
+If scan-directory-with-metascan helped you, how about buying me a beer? Use the donate button below. THANK YOU!
+
+[![Donate](https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KT462HRW7XQ3J)
+
+
+[chall32.blogspot.com]: http://chall32.blogspot.com
+[www.metascan-online.com]: https://www.metascan-online.com/en/about
+[Public API]: https://www.metascan-online.com/en/public-api
+[API definitions]: https://www.metascan-online.com/en/public-api#/definitions
+[SABnzbd postprocessing script]: http://wiki.sabnzbd.org/user-scripts
+[changelog]: https://github.com/chall32/scan-directory-with-metascan/blob/master/ChangeLog.txt
---
+++ scan-directory-with-metascan.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+'''
+__author__ = "Chris Hall"
+__copyright__ = "Copyright 2015, Chris Hall"
+__credits__ = ["Sander - http://forums.sabnzbd.org/viewtopic.php?t=13268"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Chris Hall"
+__email__ = "twitter.com/chall32"
+__status__ = "Production"
+#
+#### PURPOSE #############################################################################################################
+#
+This script will use https://www.metascan-online.com to scan a directory of files.
+It does this by calcuating an MD5 hash (https://en.wikipedia.org/wiki/MD5) and searching for the MD5 hash using
+Metascan's public API. 
+
+If the file is not found on Metascan's site, is deemed to have a suspect extension (see extlist below) and is smaller
+than the maximum file size for uploading to Metascan (see maxfilesize below), the script will upload a copy of the file
+for scanning via Metascan's public API.  It will then recheck Metascan for the results of the scan using the file's MD5
+hash. 
+
+The script is intially meant to be used as SABnzbd postprocessing script.
+#
+#### REQUIREMNTS, USE, EXAMPLE ###########################################################################################
+#
+REQUIREMNETS:
+A free Metascan Public API key. This can obtained from https://www.metascan-online.com/en/public-api
+Once obtained, paste this into myapikey below
+
+USE:
+scan-directory-with-metascan.py <Directory to be Scanned>
+
+EXAMPLE:
+python scan-directory-with-metascan.py C:\Files
+#
+##########################################################################################################################
+'''
+import os
+import hashlib
+import sys
+import urllib2
+import time
+import json
+#
+# Initilize counts for final wrapup
+cleanfound = 0
+unkwnfound = 0
+susipfound = 0
+virusfound = 0
+problemhlp = 0
+#
+#### VARIABLES ###########################################################################################################
+#                                                                  
+extlist = ['.exe','.com','.vbs','.vbe','.js','.jse','.jar','.wsf', # Extensions of suspect files that should be uploaded
+           '.wsh','.msc','msi','.pif','.scr','.hta','.apk','.jpg', # to Metascan (extensions should be dot lowercase)
+           '.jpeg']                                                #  
+maxfilesize = '10000000' 					   # Maximum file size to be uploaded to Metascan (bytes)
+myapikey = ''			                                   # Metascan API key
+hashlookupurl = 'https://hashlookup.metascan-online.com/v2/hash/'  # Metascan API hash lookup URL
+hashresulturl = 'https://www.metascan-online.com/scanresult/hash/' # Metascan API hash result URL
+fileuploadurl = 'https://scan.metascan-online.com/v2/file'         # Metascan API file scan upload URL
+secondsforscan = '30'                                              # Time to wait for scan of uploaded file (seconds)
+#
+##########################################################################################################################
+# Function to calculate md5 hash of a file
+def md5_of_file(fullfilename):
+	md5 = hashlib.md5()
+	with open(fullfilename,'rb') as f: 
+	    for chunk in iter(lambda: f.read(8192), b''): 
+		 md5.update(chunk)
+	return md5.hexdigest().upper()
+#   
+# Funtion to post md5 hash of a file to Metascan
+def metascan_hash(md5):
+        md5url = hashlookupurl + md5
+        requestToSeeIfScannedBefore = urllib2.Request(md5url)
+        requestToSeeIfScannedBefore.add_header('apikey', myapikey)
+        try:
+                hashresults = urllib2.urlopen(requestToSeeIfScannedBefore).read().decode("utf-8")
+        except:
+                print "Problem contacting Metascan.  Please check \n"
+        return hashresults
+#   
+# Funtion to read and log Metascan md5 hash lookup results 
+def process_hash_lookup(hashlookup):
+	if '"scan_all_result_a":"Infected"' in hashlookup:
+		print "Virus found in " + file
+		print "See URL: " + hashresulturl + md5 + "\n"
+		newfilename = fullfilename + '__INFECTED'
+		os.rename(fullfilename, newfilename)
+		global virusfound
+		virusfound += 1000
+		uploadfile = False
+	elif '"scan_all_result_a":"Suspicious"' in hashlookup:
+		print "Suspicious file " + file
+		print "See URL: " + hashresulturl + md5 + "\n"
+		newfilename = fullfilename + '__SUSPICIOUS'
+		os.rename(fullfilename, newfilename)
+		global susipfound
+		susipfound += 100
+		uploadfile = False
+	elif '"scan_all_result_a":"Clean"' in hashlookup:
+		print "No Virus found in" + file
+	 	print "See URL: " + hashresulturl + md5 + "\n"
+                global cleanfound
+                cleanfound += 10
+	 	uploadfile = False
+	elif '"Not Found"' in hashlookup:
+		print "File not seen before" + file
+	 	print "See URL: " + hashresulturl + md5 + "\n"
+	 	global unkwnfound
+	 	unkwnfound += 1
+	 	uploadfile = False
+	 	if extension in extlist:
+                        size = os.path.getsize(fullfilename)
+                        if size <= maxfilesize:
+                                uploadfile = True
+        else:
+                print "**** Problem Scanning!! **** \n"
+                print hashresults + "\n"
+                global problemhlp
+                problemhlp += 10000
+                uploadfile = False
+        return uploadfile
+#
+# Funtion to upload file to Metascan if file extension appears in suspect list (extlist)
+def metascan_file(fullfilename):
+        print "File is on risky file list.  Uploading copy to Metascan for definitive analysis \n"
+        # Upload file
+        contents = open(fullfilename).read()
+        request = urllib2.Request(fileuploadurl, contents)
+        request.add_header('apikey', myapikey)
+        request.add_header('filename', file)
+        try:
+                response = urllib2.urlopen(request).read().decode("utf-8")
+                decoded_response = json.loads(response)
+                print "File uploaded. Waiting " + secondsforscan + " seconds . . . \n"
+                time.sleep(float(secondsforscan))
+        except:
+                print "Problem contacting Metascan.  Please check \n"
+#
+#### MAIN #################################################################################################################
+# Check launch parameters. Launch with valid directory name
+if len(sys.argv) < 2:
+        sys.exit('Usage: %s directory-name' % sys.argv[0])
+dirname = sys.argv[1]
+if not os.path.exists(dirname):
+        sys.exit('ERROR: Directory not found' % sys.argv[1])
+# Main Loop
+for root, dirs, files in os.walk(dirname):
+        for file in files:
+                extension = os.path.splitext(file)[1].lower()
+                fullfilename = os.path.join(root, file)
+                md5 = md5_of_file(fullfilename)
+                hashresults = metascan_hash(md5)
+                uploadfile = process_hash_lookup(hashresults)
+                if uploadfile:
+                   metascan_file(fullfilename)
+                   hashresults = metascan_hash(md5)
+                   process_hash_lookup(hashresults)
+# Final wrapup for SABnzbd WebGUI
+finalwrapup = ["cleanfound","unkwnfound","susipfound","virusfound","problemhlp"]
+finalscore = max(finalwrapup, key = locals().get)
+if finalscore == "virusfound":
+	print "**** VIRUS FOUND!! ****"
+elif finalscore == "susipfound":
+        print "**** SUSPICIOUS FILE!! ****"
+elif finalscore == "unkwnfound":
+        print "File(s) not known to Metascan"
+elif finalscore == "cleanfound":
+        print "All OK - no virus found"
+elif finalscore == "problemhlp":
+        print "**** Problem Scanning!! ****"
+else:
+        print "**** Something very wrong!! ****"
